0413351c9edd15ac60374151c508cf3d3a720a0c
Kenneth <k.chevezramirez@gmail.com>
2018-06-17 18:23:43 -0600

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:mdocena5@ds163680.mlab.com:63680/cafe-kenneth';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
