ea545a0bb660630d1892b4280e2ed0c3c61e08b9
Kenneth <k.chevezramirez@gmail.com>
2018-06-17 18:07:52 -0600

    Acceso y manejo de datos con mongodb

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,24 @@
 // ========================
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ========================
+// Entorno
+// ========================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ========================
+// Base de datos
+// ========================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:mdocena5@ds163680.mlab.com:63680/cafe-kenneth';
+}
+
+process.env.URLDB = urlDB;
